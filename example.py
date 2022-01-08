90d0a344a1e9af4c2ee1f2176ccb6c271b13a64e
Georgi Karaliev <georgi.nikolov733@gmail.com>
2022-01-08 19:16:11 +0200

    Check

--- example.py
+++ example.py
@@ -1,20 +1,10 @@
-import math
-lst = []
-while True:
-    x = input()
-    if len(x)==0:
-        break
-    lst.append(x.split(' '))
-length = 0
-width = 0
-for item in lst:
-        if item[0] == 'UP':
-            length += int(item[1])
-        if item[0] == 'DOWN':
-            length -= int(item[1])
-        if item[0] == 'LEFT':
-            width -= int(item[1])
-        if item[0] == 'RIGHT':
-            width += int(item[1])
-distance = int(math.sqrt(length**2 + width**2))
-print(distance)
+for i in range(1, 100):
+
+    if i%3 == 0 and i%5 ==0:
+        print('FizzBuzz')
+    elif i%3 == 0:
+        print('Fizz')
+    elif i%5 ==0:
+        print('Buzz')
+    else:
+        print(i)
