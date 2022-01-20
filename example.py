5ed124ed9d2fabe824f196fd6f691d1e7f9676e3
Georgi Karaliev <georgi.nikolov733@gmail.com>
2022-01-20 16:21:16 +0200

    MS5

--- example.py
+++ example.py
@@ -4,12 +4,12 @@ for i in range(1, 100):
 
     if i % 3 == 0 and i % 5 == 0:
         print('FizzBuzz')
-        time.sleep(600)
+        time.sleep(5)
     elif i % 3 == 0:
         print('Fizz')
-        time.sleep(600)
+        time.sleep(5)
     elif i % 5 == 0:
         print('Buzz')
-        time.sleep(600)
+        time.sleep(5)
     else:
         print(i)
