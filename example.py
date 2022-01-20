4cbf422e1705fc201ed7feebfe10c4e12d5199b8
Georgi Karaliev <georgi.nikolov733@gmail.com>
2022-01-20 16:09:25 +0200

    MS5

--- example.py
+++ example.py
@@ -1,10 +1,15 @@
+import time
+
 for i in range(1, 100):
 
     if i % 3 == 0 and i % 5 == 0:
         print('FizzBuzz')
+        time.sleep(600)
     elif i % 3 == 0:
         print('Fizz')
+        time.sleep(600)
     elif i % 5 == 0:
         print('Buzz')
+        time.sleep(600)
     else:
         print(i)
